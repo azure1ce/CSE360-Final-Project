5a4b2c8ddacf075a1ff8ea97fb254297c1878e9f
azure1ce <43428732+azure1ce@users.noreply.github.com>
2018-11-14 10:02:01 -0800

    Add files via upload

--- about.java
+++ about.java
@@ -15,7 +15,9 @@ public class about extends JPanel{
 		temp += "This program will take in the activities of a network diagram as input.\n";
 		temp += "It will then use the inputs to find the paths in the network diagram.\n";
 		temp += "Once the paths have been found, it will display the results to the user.\n";
-		temp += "The paths will listed based on the duration, with the first paths being the ones with the shortest duration.";
+		temp += "The paths will listed based on the duration, with the first paths being the ones with the shortest duration.\n";
+		temp += "\nRepresented by Team P:\nEric Tan\nJingming Tan\nZachary Simmons\nZhiheng Zhang\\n\n";
+		temp += "\nV 2.0.1\n\n";
 		about1.setText(temp);
 		
 		add(about1);
